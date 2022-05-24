1f202ca0c17b8e43169a56f1c9bc33877edaf331
祁冬 <635877990@qq.com>
2022-05-24 19:06:35 +0800

    1.添加重置密码的界面

---
+++ Authing.Guard.WPF/Authing.Guard.WPF/Views/LoginView/ResetPasswordView.xaml.cs
@@ -0,0 +1,167 @@
+﻿using Authing.ApiClient.Domain.Model;
+using Authing.Guard.WPF.Factories;
+using Authing.Guard.WPF.Infrastructures;
+using Authing.Guard.WPF.Utils;
+using Authing.Guard.WPF.Utils.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authing.Guard.WPF.Views.LoginView
+{
+    /// <summary>
+    /// ResetPasswordView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ResetPasswordView : UserControl
+    {
+        private IRegexService m_RegexService;
+
+        public ResetPasswordView()
+        {
+            InitializeComponent();
+            m_RegexService = new RegexService();
+        }
+
+        private async void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            JudgeInput();
+
+            CommonMessage commonMessage = null;
+
+            if (m_RegexService.IsMail(tbAccount.Text))
+            {
+                commonMessage = await AuthClient.Instance.ResetPasswordByEmailCode(tbAccount.Text, tbCode.Text, tbPassword.Password);
+            }
+            else if (m_RegexService.IsPhone(tbAccount.Text))
+            {
+                commonMessage = await AuthClient.Instance.ResetPasswordByPhoneCode(tbAccount.Text, tbCode.Text, tbPassword.Password);
+            }
+
+            if (commonMessage != null)
+            {
+                if (commonMessage.Code.Value == 200)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private async void btnSendCode_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbAccount.Text))
+            {
+                tbAccount.Warn = true;
+                tbAccountRemind.Text = "手机号 / 邮箱未填写";
+                tbAccountRemind.Visibility = Visibility.Visible;
+                return;
+            }
+
+            if (!m_RegexService.IsPhone(tbAccount.Text) || !m_RegexService.IsMail(tbAccount.Text))
+            {
+                tbAccount.Warn = true;
+                tbAccountRemind.Text = "请输入正确的手机号或邮箱";
+                tbAccountRemind.Visibility = Visibility.Visible;
+                return;
+            }
+
+            CommonMessage commonMessage = null;
+
+            if (m_RegexService.IsPhone(tbAccount.Text))
+            {
+                commonMessage = await AuthClient.Instance.SendSmsCode(tbAccount.Text);
+            }
+            else if (m_RegexService.IsMail(tbAccount.Text))
+            {
+                commonMessage = await AuthClient.Instance.SendSmsCode(tbAccount.Text);
+            }
+
+            if (commonMessage.Code.Value != 200)
+            {
+                throw new Exception(commonMessage.Message);
+            }
+
+        }
+
+        private bool JudgeInput()
+        {
+            bool flag = true;
+
+            if (string.IsNullOrWhiteSpace(tbAccount.Text))
+            {
+                tbAccount.Warn = true;
+                tbAccountRemind.Visibility = Visibility.Visible;
+                BeginStoryboard(tbAccount);
+                flag = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbCode.Text))
+            {
+                tbCode.Warn = true;
+                tbCodeRemind.Visibility = Visibility.Visible;
+                BeginStoryboard(tbPassword);
+                flag = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbPassword.Password))
+            {
+                PasswordBoxHelper.SetWarn(tbPassword, true);
+                tbPasswordRemind.Visibility = Visibility.Visible;
+                BeginStoryboard(tbPassword);
+                flag = false;
+            }
+            else
+            {
+                //判断密码有效性
+                if (!m_RegexService.PasswordMatch(tbPassword.Password))
+                {
+                    PasswordBoxHelper.SetWarn(tbPassword, true);
+                    tbPasswordRemind.Text = "密码长度至少为 6 位，且要包含英文、数字与符号中的两种";
+                    tbPasswordRemind.Visibility = Visibility.Visible;
+                    BeginStoryboard(tbPassword);
+                    flag = false;
+                }
+            }
+
+            return flag;
+        }
+
+        private void BeginStoryboard<T>(T control) where T : Control
+        {
+            Storyboard storyboard = (Storyboard)control.Resources["ShakeStoryboard"];
+            Storyboard.SetTarget(storyboard.Children.ElementAt(0) as DoubleAnimationUsingKeyFrames, control);
+            storyboard.Begin();
+        }
+
+        private void tbCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbCode.Warn = false;
+            tbCodeRemind.Visibility = Visibility.Collapsed;
+        }
+
+        private void tbAccount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbAccount.Warn = false;
+            tbAccountRemind.Visibility = Visibility.Collapsed;
+        }
+
+        private void tbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBoxHelper.SetWarn(tbPassword, false);
+            tbPasswordRemind.Visibility = Visibility.Collapsed;
+        }
+    }
+}
--- Authing.Guard.WPF/Authing.Guard.WPF/Views/LoginView/SMSCodeLoginView.xaml.cs
+++ Authing.Guard.WPF/Authing.Guard.WPF/Views/LoginView/SMSCodeLoginView.xaml.cs
@@ -2,6 +2,8 @@
 using Authing.ApiClient.Types;
 using Authing.Guard.WPF.Factories;
 using Authing.Guard.WPF.Services;
+using Authing.Guard.WPF.Utils;
+using Authing.Guard.WPF.Utils.Impl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,27 +27,41 @@ namespace Authing.Guard.WPF.Views.LoginView
     /// </summary>
     public partial class SMSCodeLoginView : UserControl
     {
+        private IWindowsAPI m_WindowsAPI;
+        private IRegexService m_RegexService;
         public SMSCodeLoginView()
         {
             InitializeComponent();
 
             Loaded += SMSCodeLoginView_Loaded;
+
+            m_WindowsAPI = new WindowsAPI();
+            m_RegexService = new RegexService();
         }
 
         private void SMSCodeLoginView_Loaded(object sender, RoutedEventArgs e)
         {
             tbPhone.Warn = false;
             tbSMSCode.Warn = false;
-            tbPhoneInfo.Visibility = Visibility.Collapsed;
-            tbSMSCodeInfo.Visibility = Visibility.Collapsed;
+            tbPhoneRemind.Visibility = Visibility.Collapsed;
+            tbSMSCodeRemind.Visibility = Visibility.Collapsed;
         }
 
         private async void btnSendSMS_Click(object sender, RoutedEventArgs e)
         {
-            if (!RegexService.IsMobile(tbPhone.Text))
+            if (string.IsNullOrWhiteSpace(tbPhone.Text))
             {
-                tbPhoneInfo.Visibility = Visibility.Visible;
                 tbPhone.Warn = true;
+                tbPhoneRemind.Text = "手机号未填写";
+                tbPhoneRemind.Visibility = Visibility.Visible;
+                return;
+            }
+
+            if (!m_RegexService.IsPhone(tbPhone.Text))
+            {
+                tbPhone.Warn = true;
+                tbPhoneRemind.Text = "输入有效的手机号";
+                tbPhoneRemind.Visibility = Visibility.Visible;
                 return;
             }
 
@@ -58,18 +75,92 @@ namespace Authing.Guard.WPF.Views.LoginView
 
         private async void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(tbSMSCode.Text))
+            if (!JudgeInput())
             {
-                tbSMSCodeInfo.Visibility = Visibility.Visible;
-                tbSMSCode.Warn = true;
                 return;
             }
+            User user = null;
 
-            User user = await AuthClient.Instance.LoginByPhoneCode(tbPhone.Text.Trim(), tbSMSCode.Text.Trim(), new RegisterAndLoginOptions());
+            user = await AuthClient.Instance.LoginByPhoneCode(tbPhone.Text, tbSMSCode.Text, new RegisterAndLoginOptions { AutoRegister = false });
+
             if (user != null)
-            {
-
+            { 
+                //用户登录成功
             }
+        }
+
+        private bool JudgeInput()
+        {
+            bool flag = true;
+
+            if (string.IsNullOrWhiteSpace(tbPhone.Text))
+            {
+                tbPhone.Warn = true;
+                tbPhoneRemind.Visibility = Visibility.Visible;
+                BeginStoryboard(tbPhone);
+
+                flag = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbSMSCode.Text))
+            {
+                tbSMSCode.Warn = true;
+                tbSMSCodeRemind.Visibility = Visibility.Visible;
+                BeginStoryboard(tbSMSCode);
+
+                flag = false;
+            }
+            if (cbAgree.IsChecked == null || cbAgree.IsChecked == false)
+            {
+                cbAgree.Foreground = new SolidColorBrush(Colors.Red);
+                BeginStoryboard(cbAgree);
+
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        private void BeginStoryboard<T>(T control) where T : Control
+        {
+            Storyboard storyboard = (Storyboard)control.Resources["ShakeStoryboard"];
+            Storyboard.SetTarget(storyboard.Children.ElementAt(0) as DoubleAnimationUsingKeyFrames, control);
+            storyboard.Begin();
+        }
+
+        private void cbAgree_Checked(object sender, RoutedEventArgs e)
+        {
+            cbAgree.Foreground = new SolidColorBrush(Colors.Black);
+            linkService.Foreground = new SolidColorBrush(Colors.MediumBlue);
+            linkPrivacy.Foreground = new SolidColorBrush(Colors.MediumBlue);
+        }
+
+        private void cbAgree_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cbAgree.Foreground = new SolidColorBrush(Colors.Red);
+            linkService.Foreground = new SolidColorBrush(Colors.Red);
+            linkPrivacy.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void linkService_Click(object sender, RoutedEventArgs e)
+        {
+            m_WindowsAPI.ShellExecute("open", linkService.NavigateUri.AbsoluteUri);
+        }
+
+        private void linkPrivacy_Click(object sender, RoutedEventArgs e)
+        {
+            m_WindowsAPI.ShellExecute("open", linkPrivacy.NavigateUri.AbsoluteUri);
+        }
+
+        private void tbSMSCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbSMSCode.Warn = false;
+            tbSMSCodeRemind.Visibility = Visibility.Collapsed;
+        }
+
+        private void tbPhone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbPhone.Warn = false;
+            tbPhoneRemind.Visibility = Visibility.Collapsed;
         }
     }
 }
