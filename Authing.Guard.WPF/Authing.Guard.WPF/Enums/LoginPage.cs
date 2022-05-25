d5b77e7465b78328c5c49c13ddc150509283aaaa
祁冬 <635877990@qq.com>
2022-05-25 16:09:08 +0800

    1.添加页面类型的枚举

---
+++ Authing.Guard.WPF/Authing.Guard.WPF/Enums/LoginPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authing.Guard.WPF.Enums
+{
+    /// <summary>
+    /// 登录界面
+    /// </summary>
+    public enum LoginPage
+    {
+        /// <summary>
+        /// 扫码登录
+        /// </summary>
+        QRCodeLogin,
+        /// <summary>
+        /// 更多登录
+        /// </summary>
+        MoreLogin,
+        /// <summary>
+        /// 忘记密码
+        /// </summary>
+        ForgetPassword,
+        /// <summary>
+        /// 立即注册
+        /// </summary>
+        Register
+    }
+}
--- Authing.Guard.WPF/Authing.Guard.WPF/Views/Main/MainView.xaml.cs
+++ Authing.Guard.WPF/Authing.Guard.WPF/Views/Main/MainView.xaml.cs
@@ -2,6 +2,7 @@
 using Authing.Guard.WPF.Enums;
 using Authing.Guard.WPF.Factories;
 using Authing.Guard.WPF.Utils;
+using Authing.Guard.WPF.Views.LoginView;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,7 +19,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Authing.Guard.WPF.Views.Main
+namespace Authing.Guard.WPF.Views.LoginView
 {
     /// <summary>
     /// MainView.xaml 的交互逻辑
@@ -26,6 +27,9 @@ namespace Authing.Guard.WPF.Views.Main
     public partial class MainView : BaseGuardControl
     {
         private IImageService m_ImageService;
+
+
+        private LoginPage m_CurrentLoginPage;//当前的页面
 
         public MainView()
         {
@@ -53,20 +57,30 @@ namespace Authing.Guard.WPF.Views.Main
                 imgLogo.Source = BitmapFrame.Create(ms, BitmapCreateOptions.None, BitmapCacheOption.Default);
             }
 
+            SimulationData();
+
             InitLoginMethod();
 
             InitRegisterMethod();
 
 
 
-        
-           
-
             //根据配置显示界面
+        }
+
+        private void SimulationData()
+        {
+
+            Config.LoginMethods = new List<LoginMethods>();
+            Config.RegisterMethods = new List<RegisterMethods>();
+
+            Config.LoginMethods.Add(LoginMethods.Password);
+            Config.LoginMethods.Add(LoginMethods.PhoneCode);
         }
 
         private void InitLoginMethod()
         {
+
             foreach (var item in Config.LoginMethods)
             {
                 if (item == LoginMethods.AD)
@@ -84,13 +98,21 @@ namespace Authing.Guard.WPF.Views.Main
                 else if (item == LoginMethods.Password)
                 {
                     //添加账号+密码登录
+                    TabItem tabItem = new TabItem();
+                    tabItem.Header = "11231";
+                    tabItem.Content = new PasswordLoginView();
+
+                    loginViewTabControl.Items.Add(tabItem);
                 }
                 else if (item == LoginMethods.PhoneCode)
                 {
                     //添加手机号验证码登录
+                    TabItem tabItem = new TabItem();
+                    tabItem.Content = new SMSCodeLoginView();
+                    loginViewTabControl.Items.Add(tabItem);
                 }
                 else if (item == LoginMethods.WxMinQr)
-                { 
+                {
                     //添加微信小程序扫码登录
                 }
             }
@@ -98,7 +120,7 @@ namespace Authing.Guard.WPF.Views.Main
 
         private void InitRegisterMethod()
         {
-            
+
             foreach (var item in Config.RegisterMethods)
             {
                 if (item == RegisterMethods.Email)
@@ -106,12 +128,20 @@ namespace Authing.Guard.WPF.Views.Main
                     //添加邮箱注册
                 }
                 else if (item == RegisterMethods.Phone)
-                { 
+                {
                     //添加手机注册
                 }
             }
         }
 
-     
+        private void btnSwitchLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnSwitchLogin.IsChecked == true)
+            {
+
+            }
+        }
+
+
     }
 }
