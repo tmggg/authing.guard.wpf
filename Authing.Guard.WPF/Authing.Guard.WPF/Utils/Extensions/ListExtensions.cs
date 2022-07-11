5b4499cffcb325d0160b7168e35ee373530ce2cc
qidong5566 <qidong5566@outlook.com>
2022-07-11 16:28:43 +0800

    feat:修改获取应用信息接口为 v2

---
+++ Authing.Guard.WPF/Authing.Guard.WPF/Utils/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authing.Guard.WPF.Utils.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<TEnum> GetEnumByEnumMember<TEnum>(this List<string> list) where TEnum : Enum
+        {
+            List<TEnum> enums = new List<TEnum>();
+
+            foreach (var item in list)
+            {
+                enums.Add(item.GetEnumByEnumMember<TEnum>());
+            }
+
+            return enums;
+        }
+    }
+}
