4206bf4336b980d34ca20848088145e7974b633b
qidong5566 <qidong5566@outlook.com>
2022-07-12 19:52:32 +0800

    fix:重写了主界面切换逻辑。

---
+++ Authing.Guard.WPF/Authing.Guard.WPF/Views/V1/Main/RegisterMainView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Authing.Guard.WPF.Models;
+using Authing.Guard.WPF.Utils;
+using Authing.Guard.WPF.Utils.Impl;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authing.Guard.WPF.Views.V1.Main
+{
+    /// <summary>
+    /// RegisterMainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class RegisterMainView : UserControl
+    {
+        private readonly GuardConfig config;
+
+        private IImageService m_ImageService;
+
+        public RegisterMainView(GuardConfig guardConfig)
+        {
+            InitializeComponent();
+
+            config = guardConfig;
+            m_ImageService = new ImageService();
+            Loaded += RegisterMainView_Loaded;
+        }
+
+        private void RegisterMainView_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(config.Logo))
+            {
+                byte[] byteArray = m_ImageService.GetImageFromResponse(config.Logo);
+
+                MemoryStream ms = new MemoryStream(byteArray);
+
+                imgLogo.Source = BitmapFrame.Create(ms, BitmapCreateOptions.None, BitmapCacheOption.Default);
+            }
+        }
+
+        private void RegReturn2Login_OnClick(object sender, RoutedEventArgs e)
+        {
+            RegContent.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ Authing.Guard.WPF/Authing.Guard.WPF/Views/V1/Main/ThirdLoginMainView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Authing.Guard.WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authing.Guard.WPF.Views.V1.Main
+{
+    /// <summary>
+    /// ThirdLoginMainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ThirdLoginMainView : UserControl
+    {
+        private ObservableCollection<SocialLogin> DemoData;
+        public ThirdLoginMainView()
+        {
+            InitializeComponent();
+
+            Loaded += ThirdLoginMainView_Loaded;
+        }
+
+        private void ThirdLoginMainView_Loaded(object sender, RoutedEventArgs e)
+        {
+            SimulationData();
+        }
+
+        private void SimulationData()
+        {
+            DemoData = new ObservableCollection<SocialLogin>();
+            DemoData.Add(new SocialLogin("https://www.qq.com", new SolidColorBrush(Colors.Red), Application.Current.Resources["QQ"] as Geometry));
+            DemoData.Add(new SocialLogin("https://www.google.com", new SolidColorBrush(Colors.Orange), Application.Current.Resources["Google"] as Geometry));
+            DemoData.Add(new SocialLogin("https://www.linkedin.cn", new SolidColorBrush(Colors.Yellow), Application.Current.Resources["Linkedin"] as Geometry));
+            DemoData.Add(new SocialLogin("https://www.weixin.com", new SolidColorBrush(Colors.Green), Application.Current.Resources["WeChat"] as Geometry));
+            DemoData.Add(new SocialLogin("https://www.facebook.com", new SolidColorBrush(Colors.Blue), Application.Current.Resources["FaceBook"] as Geometry));
+            DemoData.Add(new SocialLogin("https://www.github.com", new SolidColorBrush(Colors.Purple), Application.Current.Resources["GitHub"] as Geometry));
+            SocialloginControl.ItemsSource = DemoData;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                if (((Button)sender).DataContext is SocialLogin)
+                {
+                    SocialLogin data = ((Button)sender).DataContext as SocialLogin;
+                    Process.Start(data.LoginUrl);
+                }
+            }
+        }
+    }
+}
