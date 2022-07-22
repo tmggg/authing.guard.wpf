b510773bdff8cf534c400a205bcbd5383326024c
qidong5566 <qidong5566@outlook.com>
2022-07-22 18:24:28 +0800

    fix；CEF 兼容性修复

--- Authing.Guard.WPF/Authing.Guard.WPF/Views/Classic/MainView/GuardMainView.xaml.cs
+++ Authing.Guard.WPF/Authing.Guard.WPF/Views/Classic/MainView/GuardMainView.xaml.cs
@@ -39,7 +39,7 @@ namespace Authing.Guard.WPF.Views.Classic.MainView
 
         public GuardMainView()
         {
-            InitializeCefSharp();
+            CollapsableChromiumWebBrowser.Init();
 
             var dic = ResourceHelper.GetLanguageDictionary(Lang.zhCn);
 
@@ -59,44 +59,6 @@ namespace Authing.Guard.WPF.Views.Classic.MainView
             Loaded += MainView_Loaded;
             Unloaded += MainView_Unloaded;
         }
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static void InitializeCefSharp()
-        {
-            return;
-
-            var settings = new CefSettings();
-
-            // Set BrowserSubProcessPath based on app bitness at runtime
-            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                   Environment.Is64BitProcess ? "x64" : "x86",
-                                                   "CefSharp.BrowserSubprocess.exe");
-
-
-            // Make sure you set performDependencyCheck false
-            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
-        }
-
-        // Will attempt to load missing assembly from either x86 or x64 subdir
-        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
-        private static Assembly Resolver(object sender, ResolveEventArgs args)
-        {
-            if (args.Name.StartsWith("CefSharp"))
-            {
-                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
-                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                       Environment.Is64BitProcess ? "x64" : "x86",
-                                                       assemblyName);
-
-                return File.Exists(archSpecificPath)
-                           ? Assembly.LoadFile(archSpecificPath)
-                           : null;
-            }
-
-            return null;
-        }
-
-
 
         private void MainView_Unloaded(object sender, RoutedEventArgs e)
         {
